121c2345bdeaeae45a9b783b6fdaf9e88eaf95b9
Max V Grushin <support@ids.spb.ru>
2024-05-20 10:32:17 +0300

    feat: добавлено бюргер меню

---
+++ src/js/layout/header.js
@@ -0,0 +1,9 @@
+const hamburger = document.querySelector(".hamburger--spin");
+const mobileNavs = document.querySelector(".header__menu-mobile");
+const overlay = document.querySelector(".header__menu-mobile--overlay");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("is-active");
+  mobileNavs.classList.toggle("is-active");
+  overlay.classList.toggle("is-active");
+});
