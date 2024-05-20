9206741e31379078adaa58820ddf2a5ac18abd54
Max V Grushin <support@ids.spb.ru>
2024-05-20 16:48:11 +0300

    fix: правки для отбражения форм на wp

--- src/js/layout/header.js
+++ src/js/layout/header.js
@@ -1,9 +1,16 @@
 const hamburger = document.querySelector(".hamburger--spin");
 const mobileNavs = document.querySelector(".header__menu-mobile");
 const overlay = document.querySelector(".header__menu-mobile--overlay");
+const search = document.querySelector(".header__search");
+
+
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("is-active");
   mobileNavs.classList.toggle("is-active");
   overlay.classList.toggle("is-active");
+});
+
+search.addEventListener("click", () => {
+  search.classList.toggle("is-active");
 });
