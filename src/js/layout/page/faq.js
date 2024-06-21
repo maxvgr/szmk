2d5e61d490a8db5033e2bcea3098ad5e60878441
Max V Grushin <support@ids.spb.ru>
2024-06-21 12:41:29 +0300

    fix: корректировка вывода аккордеона

--- src/js/layout/page/faq.js
+++ src/js/layout/page/faq.js
@@ -1,7 +1,16 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 
-new Accordion('.accordion-container');
+// new Accordion('.accordion-container');
+
+const element = document.querySelector("section.faq");
+
+if (element) {
+  new Accordion('.accordion-container');
+}
+
+
+// new Accordion('.accordion-container-vacancy');
 
 // new Accordion('.accordion-container1');
 
